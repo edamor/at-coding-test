4cea944f5665aa44ab424680a04378b78e4d276a
Ed Amor <amor.edlouise@gmail.com>
2022-10-25 16:38:04 +0800

    Change env import path

--- src/app/service/finnhub.service.ts
+++ src/app/service/finnhub.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
-import { environment as ENV } from "src/environments/environment";
+import { environment as ENV } from "../../environments/environment";
 import { filter, from, map, mergeMap, Observable } from "rxjs";
 import {
     FinnhubQuoteLookupResponse, FinnhubSentimentLookupResponse,
